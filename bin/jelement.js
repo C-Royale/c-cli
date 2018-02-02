40bb70d2344a9b44f4e2f942d9852a80999a933e
my9074 <313231068@qq.com>
2018-02-02 17:49:54 +0800

    Split the module into the lib directory.

--- bin/jelement.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-
-var program = require('commander');
-
-program
-  .version(require('../package').version)
-  .command('create', 'generate a new bussiness component template').alias('c')
-  .parse(process.argv);
